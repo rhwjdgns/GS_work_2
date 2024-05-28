d7bb5bf790b59be8f3d4b6ce234dd1a3b0a09bf3
rhwjd <rhwjdgns222@naver.com>
2024-05-28 16:45:54 +0900

    회원가입부분 디테일 (유효성 검사) 완성

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -7,6 +7,8 @@ import authMiddleware from "../middlewares/auth.middleware.js";
 import { Prisma } from "@prisma/client";
 
 const router = express.Router();
+const emailRegex = /^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/;
+const minLength = 6;
 
 // 사용자 회원가입 API
 router.post("/sign-up", async (req, res, next) => {
@@ -19,6 +21,17 @@ router.post("/sign-up", async (req, res, next) => {
   if (isExistUser) {
     return res.status(409).json({ message: "이미 존재하는 이메일 입니다." });
   }
+  if (!emailRegex.test(email)) {
+    return res
+      .status(400)
+      .json({ message: "이메일은 영어 소문자와 숫자 조합으로만 이루어져야 합니다." });
+  }
+  if (password.length < minLength) {
+    return res.status(400).json({ message: `password는 ${minLength}자 이상이어야 합니다.` });
+  }
+  if (password !== passwordCheck) {
+    return res.status(400).json({ message: "password는 passwordCheck와 일치해야 합니다." });
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.users.create({
@@ -28,9 +41,14 @@ router.post("/sign-up", async (req, res, next) => {
       passwordCheck,
       name,
     },
+    select: {
+      userId: true,
+      email: true,
+      name: true,
+    },
   });
 
-  return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+  return res.status(201).json({ data: user });
 });
 
 // 사용자 로그인 API
