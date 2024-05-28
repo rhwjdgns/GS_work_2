721c02d8ff5017403c081870440664bb63cba1ec
rhwjd <rhwjdgns222@naver.com>
2024-05-28 10:58:43 +0900

    캐릭터 삭제

--- src/routes/characters.router.js
+++ src/routes/characters.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/characters", authMiddleware, async (req, res, next) => {
   const { userId } = req.user;
   const { name, health, power, money } = req.body;
-  const character = await prisma.Characters.create({
+  const character = await prisma.characters.create({
     data: {
       UserId: userId,
       name,
@@ -19,6 +19,31 @@ router.post("/characters", authMiddleware, async (req, res, next) => {
   });
 
   return res.status(201).json({ data: character });
+});
+
+// 캐릭터 삭제
+router.delete("/characters/:characterId", authMiddleware, async (req, res, next) => {
+  try {
+    const characterId = parseInt(req.params.characterId, 10);
+    const character = await prisma.characters.findUnique({
+      where: {
+        CharacterId: characterId,
+      },
+    });
+
+    if (!character) {
+      return res.status(404).json({ message: `캐릭터 ID:${characterId} 를 찾을수 없습니다.` });
+    }
+
+    await prisma.characters.delete({
+      where: {
+        CharacterId: characterId,
+      },
+    });
+    return res.status(200).json({ message: "캐릭터 삭제 성공" });
+  } catch (error) {
+    res.status(500).json({ message: "캐릭터 삭제 실패" });
+  }
 });
 
 export default router;
